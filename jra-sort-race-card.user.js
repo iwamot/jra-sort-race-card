83a487e081a10c24261ab8e448e26d7a65488f1c
iwamot <hello@iwamot.com>
2016-10-03 01:06:17 +0900

    「馬番で並べる」ボタンを追加

--- jra-sort-race-card.user.js
+++ jra-sort-race-card.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         JRA - Sort Race Card
 // @namespace    http://iwamot.com/
-// @version      0.0.1
+// @version      0.0.2
 // @author       IWAMOTO Takashi <hello@iwamot.com> http://iwamot.com/
 // @description  JRAの出馬表を並べ替えできるようにします。
 // @include      http://www.jra.go.jp/JRADB/accessD.html
@@ -13,17 +13,30 @@
 (function(){
     $(document).on('click', '.jra-src-up', function(event){
         var selectedRow = $(event.target).closest('tr');
-        selectedRow.insertBefore(selectedRow.prev('tr.jra-src-row'));
+        selectedRow.insertBefore(selectedRow.prev('.jra-src-row'));
     });
 
     $(document).on('click', '.jra-src-down', function(event){
         var selectedRow = $(event.target).closest('tr');
-        selectedRow.insertAfter(selectedRow.next('tr.jra-src-row'));
+        selectedRow.insertAfter(selectedRow.next('.jra-src-row'));
     });
 
-    $('.raceDetailList .hNumber').each(function(){
+    $(document).on('click', '#jra-src-sort-by-number', function(){
+        var sortedRows = $('.jra-src-row').clone(true).sort(function(a, b){
+            return ($(a).data('jra-src-number') < $(b).data('jra-src-number')) ? -1 : 1;
+        });
+        $('.jra-src-row').each(function(i){
+            $(this).replaceWith(sortedRows[i]);
+        });
+    });
+
+    $('.raceDetailList .hNumber').each(function(i){
         $(this).prepend('<p><input class="jra-src-up" type="button" value="↑"></p>')
                .append('<p><input class="jra-src-down" type="button" value="↓"></p>')
-               .closest('tr').addClass('jra-src-row');
+               .closest('tr').addClass('jra-src-row').data('jra-src-number', i + 1);
     });
+
+    var div = $('<div>').css({'position': 'fixed', 'right': '5px', 'bottom': '5px'});
+    div.append('<input id="jra-src-sort-by-number" type="button" value="馬番で並べる">');
+    $('body').append(div);
 })();
